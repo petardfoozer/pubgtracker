6946c2b1fa59b0e75a942cb786a0a0d26959914d
Pete <peter.cageao@gmail.com>
2020-02-27 15:21:08 +0000

    overwriting all files

---
+++ private/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+namespace Models;
+
+class User{
+
+	/**
+	 * If the user is acting as themselves or delegating as another user
+	 * @var boolean
+	 */
+	public $acting_as_self = false;
+
+
+}
+?>
---
+++ public/index.php
@@ -0,0 +1,2 @@
+<?php
+require_once(dirname(__DIR__).'/vendor/surebert/framework/src/sb/Gateway.php');
